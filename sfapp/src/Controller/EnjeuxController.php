ff58c9738d7c82e2481f8747d59637d691733f4e
Altaks <altair61.dev@gmail.com>
2023-12-07 20:25:20 +0100

    Ajout de la page des enjeux

---
+++ sfapp/src/Controller/EnjeuxController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EnjeuxController extends AbstractController
+{
+    #[Route('/enjeux', name: 'app_enjeux')]
+    public function index(): Response
+    {
+        return $this->render('enjeux/index.html.twig', [
+            'controller_name' => 'EnjeuxController',
+        ]);
+    }
+}
