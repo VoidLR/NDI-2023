82a930ebc0846c3fd16034ef1e74e270c74ab9de
Adrien Panis <adrien.panis@etudiant.univ-lr.fr>
2023-12-08 05:26:22 +0100

    corriger fixtures

--- sfapp/src/DataFixtures/AppFixtures.php
+++ sfapp/src/DataFixtures/AppFixtures.php
@@ -68,20 +68,6 @@ class AppFixtures extends Fixture
         $player10->setScore(950);
         $manager->persist($player10);
 
-        $question1 = new Question("Les chats sont-ils des animaux domestiques ?", "Oui", "Non", "Peut-être", "Je ne sais pas", 1);
-        $manager->persist($question1);
-
-        $question2 = new Question("Quel est le nom de la planète sur laquelle nous vivons ?", "Mars", "Vénus", "Terre", "Jupiter", 3);
-        $manager->persist($question2);
-
-        $question3 = new Question("Parmi ces animaux, lequel est un mammifère ?", "Le requin", "Le dauphin", "Le poisson rouge", "Le crocodile", 2);
-        $manager->persist($question3);
-
-        $question4 = new Question("Quel est le nom de la capitale de la France ?", "Paris", "Lyon", "Marseille", "Toulouse", 1);
-        $manager->persist($question4);
-
-        $manager->flush();
-
         //Questions Quizz
 
         $question = new Question(
